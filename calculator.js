c12a59adb9ffae5b58d19f1964682a491a97f660
yogasripalani <yogasripalani4@gmail.com>
2026-01-22 10:43:20 +0530

    Add basic calculator functions to calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,31 @@
-hey
+function calculate(num1, num2, operation) {
+  switch (operation) {
+    case "add":
+      return num1 + num2;
+
+    case "subtract":
+      return num1 - num2;
+
+    case "multiply":
+      return num1 * num2;
+
+    case "divide":
+      if (num2 === 0) {
+        return "Error: Division by zero";
+      }
+      return num1 / num2;
+
+    case "square":
+      return num1 ** 2;
+
+    case "squareRoot":
+      if (num1 < 0) {
+        return "Error: Cannot calculate square root of negative number";
+      }
+      return Math.sqrt(num1);
+
+    default:
+      return "Error: Invalid operation";
+  }
+}
+
